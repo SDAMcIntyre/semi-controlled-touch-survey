e97f9c8730ceaaac1c0c7e29d5aafbc17a3d2354
S McIntyre <sdamcintyre+git@gmail.com>
2024-02-27 13:55:45 +0100

    cleaned up for Ilona

--- clean_maxqda_codes.R
+++ clean_maxqda_codes.R
@@ -7,17 +7,19 @@ maxqda_data_file <- "/Users/sarmc72/Library/CloudStorage/OneDrive-Linköpingsun
 maxqda_data <- read_excel(maxqda_data_file)
 
 # processed data from qualtrics
-touch_data_file <- "/Users/sarmc72/Library/CloudStorage/OneDrive-Linköpingsuniversitet/projects - in progress/semi-controlled social touch/online survey/analysis/Processed Data/touch_data.txt"
+touch_data_file <- "Processed Data/touch_data.txt"
 touch_data <- read_tsv(touch_data_file)
 
 n <- 992
 seg <- maxqda_data$Segment[n]
 person <- maxqda_data$`Document name`[n]
 
+# this code doesn't work
 touch_data %>% 
   # mutate(found = str_detect(, seg))
   mutate(found = across(Social_self:Emotional_touch, ~ str_detect(.x, seg))) %>% 
   View()
+
 
 var_name_codes <- str_remove(names(maxqda_data), ".*> ") %>% str_remove(., "Code: ")
 Code_var_codes <- str_remove(unique(maxqda_data$Code), ".*> ")
@@ -28,8 +30,8 @@ setdiff(Code_var_codes, var_name_codes)
 code_list <- intersect(var_name_codes, Code_var_codes)
 
 video_questions <- names(maxqda_data)[
-  str_detect(names(maxqda_data), "Code: [0-9]+_") #& 
-    #!str_detect(names(maxqda_data), "Autocode")
+  str_detect(names(maxqda_data), "Code: [0-9]+_") & 
+    !str_detect(names(maxqda_data), "Autocode")
   ]
 
 # includes sub-codes
@@ -42,17 +44,21 @@ maxqda_data %>%
   filter(Code == "RELATIONAL INTIMACY > Situational > What > Annoy / Tease") %>% View()
 
 
+# find video 8 segments that were coded with annoy /tease
+
+video_8_questions <- video_questions[str_detect(video_questions, " 8_")]
+
 annoy_data <- maxqda_data %>% 
   filter(Code == "RELATIONAL INTIMACY > Situational > What > Annoy / Tease") %>% 
   # mutate(freq = rowSums(pick(any_of(video_questions)))) %>% 
-  select(c("Document name", "Code", "Segment", video_questions)) 
+  select(c("Document name", "Code", "Segment", video_8_questions)) 
 
 
 annoy_no_video <- annoy_data %>% 
 # maxqda_data %>% 
-  select(c("Document name", "Segment", video_questions)) %>% 
+  select(c("Document name", "Segment", video_8_questions)) %>% 
   pivot_longer(
-    cols = video_questions,
+    cols = video_8_questions,
     names_to = "Video_Q",
     values_to = "Coded"
   ) %>% 
